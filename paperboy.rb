31742a4d567b64579a57488b5ca907f9eec898d8
Keira Rochester <keira@keira.local>
2016-06-02 15:28:03 -0400

    Paperboy exercise

--- paperboy.rb
+++ paperboy.rb
@@ -1,8 +1,9 @@
 #Create class called Paperboy.
-#Everything here will apply to all paperboys
+#Everything here will apply to all Paperboys
 class Paperboy
 
-  attr_reader :name, :quota, :experience, :side, :earnings
+  attr_accessor :name, :quota, :experience, :side
+  attr_reader :earnings
 
   def initialize(name, quota, experience, side, earnings)
     @name = name
@@ -10,6 +11,18 @@ class Paperboy
     @experience = experience
     @side = side
     @earnings = earnings
+  end
+
+  def paperboy_quota
+    quota = (experience / 2) + 50
+  end
+
+  def paperboy_deliver(start_address, end_address)
+
+  end
+
+  def paperboy_report
+
   end
 
 end
